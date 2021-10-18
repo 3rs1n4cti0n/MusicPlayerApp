88bfeb0e2c5ff7a26b600d612943e9ee424c4638
Ersan Tzampaz Chatip <hatipersan@gmail.com>
2021-10-18 16:55:27 +0300

    Designed a better UI for the music player app.

--- lib/App Pages/home_page.dart
+++ lib/App Pages/home_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -10,71 +13,97 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[700],
       body: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            flex: 1,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Flexible(
-                  child: Container(
-                    height: double.infinity,
-                    color: Colors.grey[900],
-                      child: Column(
-                        children: [
-                          IconButton(
-                              onPressed: (){},
-                              icon: Icon(
-                                  Icons.home,
-                                  color: Colors.grey[600],
-                                  size: 60,
-                              )
-                          ),
-                          SizedBox(height: 30,),
-                          IconButton(
-                              onPressed: (){},
-                              icon: Icon(
-                                Icons.account_circle,
-                                color: Colors.grey[600],
-                                size: 60,
-                              )
-                          ),
-                        ],
-                      ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 5,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Flexible(
-                  child: Container(
-                    height: double.infinity,
-                    color: Colors.grey[800],
-                    child: ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context,index){
-                        return Card(
-                          color: Colors.grey[700],
-                          elevation: 2,
-                          shadowColor: Colors.white,
-                          margin: EdgeInsets.symmetric(vertical: 10,horizontal: 5),
-                          child: Text('demo thingy')
-                        );
-                      },
-                    ),
+          Flexible(
+            child: Container(
+              color: Colors.amber,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.home_outlined),
+                    label: Text(
+                      'Home',
+                      overflow: TextOverflow.ellipsis,
                     ),
                   ),
-              ],
+                  Divider(
+                    height: 0,
+                  ),
+                  TextButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.account_box_outlined),
+                    label: Text(
+                      'Profile',
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Divider(
+                    height: 0,
+                  ),
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.settings),
+                      label: Text(
+                        'Settings',
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                ],
+              ),
             ),
           ),
+          Flexible(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    color: Colors.amber[300],
+                    child: Wrap(
+                      alignment: WrapAlignment.spaceEvenly,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Albums',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Playlists',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                        VerticalDivider(
+                          width: 0,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Author',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:music_player_app/App%20Pages/home_page.dart';
 import 'package:music_player_app/App%20Pages/loading.dart';
 import 'package:music_player_app/App%20Pages/profile.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/home',
-    routes: {
-      '/': (context) => LoadingPage(),
-      '/home': (context) => Home(),
-      '/profile': (context) => Profile(),
-    },
+  runApp(SafeArea(
+    top: true,
+    child: MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => const LoadingPage(),
+        '/home': (context) => const Home(),
+        '/profile': (context) => const Profile(),
+      },
+    ),
   ));
 }
